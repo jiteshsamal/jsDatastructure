43145482806bcf5dfab3902f8cd0b647726be74c
Jitesh samal <jitesh.samal@gmail.com>
2018-09-16 09:24:06 -0400

    implemented stack.

---
+++ Stack/stack.js
@@ -0,0 +1,32 @@
+export class Stack {
+    items=[];
+        constructor() {
+        }
+
+        push(element) {
+            
+            this.items.push(element);
+        }
+
+        pop() {
+            if (this.items.length == 0)
+                return "Underflow";
+            return this.items.pop();
+        }
+
+        peek() {
+            return this.items[this.items.length - 1];
+        }
+    
+        isEmpty() {
+            return this.items.length == 0;
+        }
+    
+        printStack() {
+            var str = "";
+            for (var i = 0; i < this.items.length; i++)
+                str += this.items[i] + " ";
+            return str;
+        }
+    
+    }
