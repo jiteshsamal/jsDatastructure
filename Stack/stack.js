f6ee4d4ffc3ca62c39c96c44c7801ff689c9926c
Jitesh samal <jitesh.samal@gmail.com>
2018-09-16 09:26:25 -0400

    added.

--- Stack/stack.js
+++ Stack/stack.js
@@ -23,7 +23,7 @@ export class Stack {
         }
     
         printStack() {
-            var str = "";
+            var str = '';
             for (var i = 0; i < this.items.length; i++)
                 str += this.items[i] + " ";
             return str;
